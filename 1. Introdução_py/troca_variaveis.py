e7b9cc09c8e09b7971f8ce6f5c6ea517e8becd5d
Gustavo Martins <118000156+G2Martins@users.noreply.github.com>
2023-08-23 23:43:39 -0300

    Create troca_variaveis.py

---
+++ 1. Introdução_py/troca_variaveis.py
@@ -0,0 +1,13 @@
+# Trocar Variáveis
+
+x = input("Insira o valor de x: ")
+y = input("Insira o valor de y: ")
+
+# Criar Variavel Temp
+
+temp = x
+x = y
+y = temp
+
+print(f"O Valor de x depois da troca: {x}")
+print(f"O Valor de y depois da troca: {y}")
