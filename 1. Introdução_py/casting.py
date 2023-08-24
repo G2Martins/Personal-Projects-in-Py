354a0d421d2b9772956a0f355b801e12169042c5
Gustavo Martins <118000156+G2Martins@users.noreply.github.com>
2023-08-23 23:33:52 -0300

    Create casting.py

---
+++ 1. Introdução_py/casting.py
@@ -0,0 +1,36 @@
+#  Conversao de valores de diferentes tipos
+
+#  Inteiros:
+
+x = int(2)
+y = int(2.8)
+z = int('2')
+
+print(x, y, z)
+
+#  Float:
+
+a = float(2.3)
+b = float(2)
+c = float('2.3')
+d = float('2')
+
+print(a, b, c, d)
+
+#  Strings
+
+t = 's1'
+r = 2
+z = 2.3
+
+print("A variavel t eh do tipo: ", type(t))
+print("A variavel r eh do tipo: ", type(r))
+print("A variavel z eh do tipo: ", type(z))
+
+g = str('s1')
+h = str(2)
+j = str(2.3)
+
+print("A variavel g eh do tipo: ", type(g))
+print("A variavel h eh do tipo: ", type(h))
+print("A variavel j eh do tipo: ", type(j))
