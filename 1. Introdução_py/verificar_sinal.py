2ff79b55ba2524bb56f258816fd23bf39f8ca134
Gustavo Martins <118000156+G2Martins@users.noreply.github.com>
2023-08-23 23:44:47 -0300

    Create verificar_sinal.py

---
+++ 1. Introdução_py/verificar_sinal.py
@@ -0,0 +1,10 @@
+# Verificar Sinal
+
+numero = int(input("Digite um numero: "))
+
+if numero > 0:
+    print(f"{numero}: Positivo")
+elif numero == 0:
+    print(f"{numero} = 0")
+else:
+    print(f"{numero}: Negativo")
